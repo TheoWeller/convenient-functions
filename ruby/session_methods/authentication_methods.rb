c05ff2527c1bd816a73acdba4e95c4c00bc6005b
Theo Weller <weller.theodor@gmail.com>
2019-07-13 15:29:47 -0600

    added session helper methods and added javascript and ruby directories

---
+++ ruby/session_methods/authentication_methods.rb
@@ -0,0 +1,30 @@
+require 'jwt'
+
+class ApplicationController < ActionController::API
+  def current_user
+      @user ||= User.find_by(id: id_from_token)
+  end
+
+  def issue_token(payload)
+    JWT.encode(payload, ENV["SECRET_KEY"], 'HS256')
+  end
+
+  def decode_token
+    begin
+      JWT.decode(@token, ENV["SECRET_KEY"], true, {algorithm: 'HS256'}).first
+    rescue JWT::DecodeError
+      return {id: nil}
+    end
+  end
+
+  def token
+    request.headers["Authenticate"]
+  end
+
+  def id_from_token
+    @decoded = decode_token
+    if @decoded
+      @decoded["id"]
+    end
+  end
+end
