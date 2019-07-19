1d170d8b93a2ee29e3c5e61fc8a63a4d033bb824
Theo Weller <weller.theodor@gmail.com>
2019-07-19 11:43:06 -0600

    added js extercises and array.sort function to sorting js

---
+++ javascript/extercises/tech_interview_prep.js
@@ -0,0 +1,52 @@
+//1. given a single lower cased word,  print true if it's a palindrome and false if it is not
+// NOTE: you cannot use the reverse method
+// Eample: hannah, kayak
+
+// 2. given a sentence output the frequency of each letter in the sentence
+// ignore whitespace
+// input: "this is a test string"
+// output: t:4,h:1,i:3 .. etc don't output " ":4, formatting of the output doesn't matter
+
+//3. given an index N find the value at that index in the fibonacci sequence
+// recursively at index N
+// fibonacci sequence = 1,1,2,3,5,8,13,21..
+// indices = 1,2,3,4,5,6,7,8
+// example: 6, output 8
+
+// 4. given a number N where 1 billion > N > 0 whole numbers only
+// output the number in words
+// ie: 384765 output three hundred eighty four thousand seven hundred sixty five
+let words = {
+  ten: 10,
+  twenty: 20,
+  thirty: 30,
+  fourty: 40,
+  fifty: 50,
+  sixty: 60,
+  seventy: 70,
+  eighty: 80,
+  ninety: 90,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+  eleven: 11,
+  twelve: 12,
+  thirteen: 13,
+  fourteen: 14,
+  fifteen: 15,
+  sixteen: 16,
+  seventeen: 17,
+  eighteen: 18,
+  nineteen: 19,
+  hundred: 100,
+  thousand: 1000,
+  million: 100000,
+  billion: 1000000000
+}
--- javascript/pure_javascript/sorting_functions.js
+++ javascript/pure_javascript/sorting_functions.js
@@ -1,4 +1,5 @@
 //Use reducers when you want to reduce the values of an arry into one single value or object.
 
 // Reminder: '.sort' mutates the array its sorting
+//Sorts by ascending order (smallest to largest)
 array.sort((a, b) => a - b);
