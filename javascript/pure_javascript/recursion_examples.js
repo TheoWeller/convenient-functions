c48192d9e33df12dfbe7fd9afdafa1cb83c990e1
Theo Weller <weller.theodor@gmail.com>
2019-07-20 18:51:35 -0600

    added list of HTTP status codes to root directory

--- javascript/pure_javascript/recursion_examples.js
+++ javascript/pure_javascript/recursion_examples.js
@@ -7,10 +7,8 @@ return reverseString(str.substr(1)) + str[0];
 }
 /*===========================================*/
 
-/*
-Factorial example.
-source: https://codeburst.io/learn-and-understand-recursion-in-javascript-b588218e87ea
-*/
+/* Factorial example.
+source: https://codeburst.io/learn-and-understand-recursion-in-javascript-b588218e87ea */
 function factorial(x) {
   // TERMINATION
   if (x < 0) return;
