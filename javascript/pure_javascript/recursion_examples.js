f0c70146919950418c1d3f97a03fa186f9ca4856
Theo Weller <weller.theodor@gmail.com>
2019-07-20 18:26:11 -0600

    added recursion example in pure_javascript

---
+++ javascript/pure_javascript/recursion_examples.js
@@ -0,0 +1,24 @@
+//RECURSIVE FUNCTIONS
+
+//Reverse String
+function reverseString(str){
+	if (str === '') return '';
+return reverseString(str.substr(1)) + str[0];
+}
+/*===========================================*/
+
+/*
+Factorial example.
+source: https://codeburst.io/learn-and-understand-recursion-in-javascript-b588218e87ea
+*/
+function factorial(x) {
+  // TERMINATION
+  if (x < 0) return;
+  // BASE
+  if (x === 0) return 1;
+  // RECURSION
+  return x * factorial(x - 1);
+}
+factorial(3);
+// 6
+/*===========================================*/
--- javascript/redux/redux_setup_deep_dive.js
+++ javascript/redux/redux_setup_deep_dive.js
@@ -14,6 +14,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom'
 
 
 //THE APPLY MIDDLEWARE ARGUMENT IS HOW ASYNC ACTIONS ARE ENABLED
