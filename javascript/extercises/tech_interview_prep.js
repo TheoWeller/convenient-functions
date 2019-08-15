f955bba20f6143ab98fd7a7e4f9739602e68faf7
Theo Weller <weller.theodor@gmail.com>
2019-08-15 13:01:18 -0400

    added js extercises

--- javascript/extercises/tech_interview_prep.js
+++ javascript/extercises/tech_interview_prep.js
@@ -1,22 +1,23 @@
-//1. given a single lower cased word,  print true if it's a palindrome and false if it is not
-// NOTE: you cannot use the reverse method
-// Eample: hannah, kayak
+/*
+1. given a single lower cased word,  print true if it's a palindrome and false if it is not
+NOTE: you cannot use the reverse method
+Eample: hannah, kayak
 
-// 2. given a sentence output the frequency of each letter in the sentence
-// ignore whitespace
-// input: "this is a test string"
-// output: t:4,h:1,i:3 .. etc don't output " ":4, formatting of the output doesn't matter
+2. given a sentence output the frequency of each letter in the sentence
+ignore whitespace
+input: "this is a test string"
+output: t:4,h:1,i:3 .. etc don't output " ":4, formatting of the output doesn't matter
 
-//3. given an index N find the value at that index in the fibonacci sequence
-// recursively at index N
-// fibonacci sequence = 1,1,2,3,5,8,13,21..
-// indices = 1,2,3,4,5,6,7,8
-// example: 6, output 8
+3. given an index N find the value at that index in the fibonacci sequence
+recursively at index N
+fibonacci sequence = 1,1,2,3,5,8,13,21..
+indices = 1,2,3,4,5,6,7,8
+example: 6, output 8
 
-// 4. given a number N where 1 billion > N > 0 whole numbers only
-// output the number in words
-// ie: 384765 output three hundred eighty four thousand seven hundred sixty five
-let words = {
+4. given a number N where 1 billion > N > 0 whole numbers only
+output the number in words
+ie: 384765 output three hundred eighty four thousand seven hundred sixty five
+// let words = {
   ten: 10,
   twenty: 20,
   thirty: 30,
@@ -50,3 +51,60 @@ let words = {
   million: 100000,
   billion: 1000000000
 }
+
+Remove duplicate values from the input and  return output that should contain all unique values.
+Input    : 1 2 1 3 2 4 1 3
+Output :  1 2 3 4
+
+
+
+Given an input of string connected with underscore like 'THIS_IS_A_TEST', transfer it to a human readable string, should remove the underscore, and capitalize the first letter of every world. 
+eg: 'THIS_IS_A_TEST' ==> 'This Is A Test'
+
+Balanced Curly brackets problem.
+Input - {{{}}}} output - false.
+Input - {{{}}} output - true. 
+
+
+In given string array find longest prefix
+Input - ['transaction','transclude','Transfer'] - output – trans
+
+
+Given a large string of words and any two given words, write a program to find the smallest number of words to traverse from the first word to reach the second word in the given string (in either direction).  For     	example, from one word to the adjacent word in the string, you traverse one (1) word. Assume the words are case sensitive.
+Example input :
+sentence : The quick brown fox jumps over the lazy
+1st word : quick
+2nd word : lazy
+Sample Output : 6
+
+
+
+
+"Symmetrical Number"
+A Symmetrical Number is the same when read from either direction (left to right or from right to left). For a given integer K, write a program to find the next symmetrical number larger than K and print it to output. Ignore leading zeros.
+Input
+The first line contains integer t, the number of test cases. Integers K are given in the next t K.
+Sample Input
+808
+2133
+Sample Output
+818
+2222
+
+
+ "Pairing Integers"
+Write a program to find all pairs of integers within an array which sum to a specified value and sort the output ascendly.
+For example: let Array be {1, 4, 45, 10, 6, -8} and sum to find be 16, "6" and "10" should be the output.
+
+
+"Count Multiple Occurrences"
+Write a program to find the duplicate characters in a string and count the number of occurrences. Ignore case differences, white spaces and special characters.
+
+
+ find the maximum longest common characters between two given string
+eg string 1 ) aabbbccddddee
+string 2) abbbcddcee
+	output : bbb
+
+write a program to find if the given string is palindrome.
+*/
